3748c457d2fad69faa86483cd63eac8dbb34243f
rafidreezwan <69665294+rafidreezwan@users.noreply.github.com>
2021-11-18 00:58:33 +0600

    Add files via upload

---
+++ M - AC or WA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+
+int main ()
+{
+    long long N,M;
+
+    cin >> N >> M;
+    if(N == M)
+    {
+        cout<<"Yes";
+    }
+    else
+        cout<<"No";
+
+
+
+    return 0;
+
+}
