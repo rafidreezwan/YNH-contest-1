6739c2ac949763f3ffc483dd2ff8374c8240b8fb
rafidreezwan <69665294+rafidreezwan@users.noreply.github.com>
2021-11-18 00:54:23 +0600

    Add files via upload

---
+++ C - Counting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+
+int main ()
+{
+     int a,b;
+        cin >> a >> b;
+    if(b>a){
+
+        cout<<((b-a)+1);
+}
+    else
+        cout<<0;
+return 0;
+
+}
