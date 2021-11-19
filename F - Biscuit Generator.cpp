60e0c6e20b645d377055b667331b472cda1cc254
rafidreezwan <69665294+rafidreezwan@users.noreply.github.com>
2021-11-19 11:33:55 +0600

    Add files via upload
    Problem Statement
    A biscuit making machine produces BB biscuits at the following moments: AA seconds, 2A2A seconds, 3A3A seconds and each subsequent multiple of AA seconds after activation.
    
    Find the total number of biscuits produced within T + 0.5T+0.5 seconds after activation.
    
    Constraints
    All values in input are integers.
    1 \leq A, B, T \leq 201≤A,B,T≤20
    Input
    Input is given from Standard Input in the following format:
    
    AA BB TT
    Output
    Print the total number of biscuits produced within T + 0.5T+0.5 seconds after activation.
    
    Sample Input 1
    3 5 7
    Sample Output 1
    10
    Five biscuits will be produced three seconds after activation.
    Another five biscuits will be produced six seconds after activation.
    Thus, a total of ten biscuits will be produced within 7.57.5 seconds after activation.
    Sample Input 2
    3 2 9
    Sample Output 2
    6
    Sample Input 3
    20 20 19
    Sample Output 3
    0

---
+++ F - Biscuit Generator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+int main()
+{
+    int a,b,t,c,x,n;
+    cin>>a>>b>>t;
+    c=t+.5;
+    x=c/a;
+    n=x*b;
+
+    cout<<n;
+    return 0;
+}
